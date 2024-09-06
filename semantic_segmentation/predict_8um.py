0f6e84a9333552ec2f12b001b971e2f477faf1a6
marinotanaka <101109473+marino423@users.noreply.github.com>
2024-09-06 21:56:17 +0900

    Update predict_8um.py
    Add visualization

--- semantic_segmentation/predict_8um.py
+++ semantic_segmentation/predict_8um.py
@@ -86,7 +86,6 @@ def mask_to_image(mask: np.ndarray):
 
 
 if __name__ == '__main__':
-    displacement_list = []
     strain_list = []
     ds_list = []
     st_list = []
@@ -168,3 +167,25 @@ if __name__ == '__main__':
         #if args.viz:
             #logging.info(f'Visualizing results for image {filename}, close to continue...')
             #plot_img_and_mask(img, mask)
+            
+    fig = plt.figure()   
+    x = strain_list
+    y1 = ds_list   
+    x2 = file_list    
+    ax = fig.add_subplot(1, 1, 1)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x,_: f'{x*10**(3):.1f}'))
+    ax.xaxis.set_major_formatter(ptick.ScalarFormatter(useMathText=True))   # こっちを先に書くこと。
+    ax.ticklabel_format(style="sci", axis="x", scilimits=(-3,-3))  
+    ax.spines['top'].set_linewidth(3)
+    ax.spines['bottom'].set_linewidth(3)
+    ax.spines['left'].set_linewidth(3)
+    ax.spines['right'].set_linewidth(3)
+    plt.plot(x, y1, linewidth=2, color = 'seagreen', label = "Dislocation")
+    y2 = st_list
+    plt.plot(x, y2, linewidth=2, color = 'darkorange', label = "Stacking Fault")
+    plt.xlabel("Strain", fontsize=26)
+    plt.ylabel("Percentage of Area", fontsize=26)
+    plt.tight_layout()
+    plt.show()
